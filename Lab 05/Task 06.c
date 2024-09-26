6aa770e2b865711eb03e4992988da56f03d2efce
Mutahir AK <178812198+Mutahir-18@users.noreply.github.com>
2024-09-26 21:04:19 +0500

    Update Task 06.c

--- Lab 05/Task 06.c
+++ Lab 05/Task 06.c
@@ -1,1 +1,6 @@
-Question of Precedence
+the answer is 30 because in the first part 5 is 
+not greater than 8 thus the ternary operator goes
+to false statement which is 1!=2<5 due to precedence 
+of operators,first it will check < or > and then not 
+equal to operator, thus 2<5 us true and 1!=2 is false
+the ternary will go to false statement which is 30 
