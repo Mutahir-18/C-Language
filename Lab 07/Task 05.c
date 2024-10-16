5b9e04accfa279845d12372f1c673757b668526f
Mutahir AK <178812198+Mutahir-18@users.noreply.github.com>
2024-10-16 17:25:58 +0500

    Update Task 05.c

--- Lab 07/Task 05.c
+++ Lab 07/Task 05.c
@@ -1,1 +1,44 @@
+Write a program to read 10 integers into an array. Then, use loops to find the
+frequency of each element in the array (how many times each number occurs).
 
+#include <stdio.h>
+int main()
+{
+    int number[10];
+    int i,j;
+    int frequency[10];
+
+    printf("Enter 10 Numbers:\n");
+    for (i=0;i<10;i++)
+    {
+        scanf("%d",&number[i]);
+        frequency[i]=-1;
+    }
+    
+    for (i=0;i<10;i++)
+    {
+        if (frequency[i] == -1)
+        {
+            int count=1;
+        
+                for (j=i+1;j<10;j++)
+                {
+                    if (number[i]==number[j])
+                    {
+                    count++;
+                    frequency[j]=0;
+                    }
+                }
+            frequency[i]=count;
+        }    
+    }
+    printf("---------\n");
+    for(i=0;i<10;i++)
+    {
+        if (frequency[i]>0)
+        {
+            printf("%d  %d\n",number[i],frequency[i]);
+        }
+    }
+return(0);
+}
