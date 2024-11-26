24c188d02320c1d3c5d6adae035991bd4ec2afd8
Mutahir AK <178812198+Mutahir-18@users.noreply.github.com>
2024-11-26 19:13:07 +0500

    Update Task 05.c

--- Lab 10/Task 05.c
+++ Lab 10/Task 05.c
@@ -1,1 +1,20 @@
+Write a recursive function bubbleSort that takes an array and its size. It performs the
+bubble sort algorithm by repeatedly comparing adjacent elements and swapping them if
+they are in the wrong order.
+
+#include <stdio.h>
+void bubbleSort(int arr[], int n) {
+
+    if (n == 1)
+        return;
+
+    for (int i = 0; i < n - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            int temp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temp;
+        }
+    }
+    bubbleSort(arr, n - 1);
+}
 
