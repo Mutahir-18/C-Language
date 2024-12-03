80d5ff1fb2ff1e38839948ade349baf59a9d7a2c
Mutahir AK <178812198+Mutahir-18@users.noreply.github.com>
2024-12-03 10:39:19 +0500

    Update Task 01.c

--- Lab 12/Task 01.c
+++ Lab 12/Task 01.c
@@ -1,1 +1,26 @@
+: Write a program that calculates the sum of all the elements in array using pointers
+(Note: Generate the array via user)
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int *arr;
+    int n;
+    int sum = 0;
+    printf("Enter the Number you wanted to enter: ");
+    scanf("%d", &n);
+    arr = (int*)malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++){
+        arr[i] = i + 1;
+        sum = sum + arr[i];
+        
+    }
+    printf("The Sum is %d", sum);
+
+    free(arr);
+    return 0;
+
+}
